2c371f2bf527116408877380210030d99b684c30
Noeloya <229524740+Noeloya@users.noreply.github.com>
2025-09-11 14:47:43 -0700

    Implemented Player Tracking with ByteTrack

--- src/detection/player_detector.py
+++ src/detection/player_detector.py
@@ -2,28 +2,30 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 import os
+import supervision as sv
 
 
 class PlayerDetector:
-    def __init__(self,  use_custom_model = False, model_path = None): #load the YOLO model
-        if use_custom_model and model_path and os.path.exists(model_path): #check if we can load custom model
-            self.model =YOLO(model_path) #load roboflow model
-            self.is_custom = True
-            print("Custom model loaded")
-        else:
-            self.model = YOLO('yolo11s.pt')#load Yolo model
-            self.is_custom = False
-            print("YOLO model loaded")
-    
+    def __init__(self, model_path):
+        if not os.path.exists(model_path): #check if we can load custom model
+            raise FileNotFoundError(f"Model path {model_path} does not exist.")
+        
+        self.model =YOLO(model_path) #load trained model
+        print("Custom model loaded")
+        self.tracker = sv.ByteTrack()
         
     def detect_players(self, frame): #detect players in the frame
-        if self.is_custom:
-            result = self.model(frame, conf = 0.35)
-        else:
-            result = self.model(frame, classes = [0], conf = 0.3)
+        result = self.model(frame, conf = 0.35)
         print(f"Detected {len(result[0].boxes)} players")
         return result[0].boxes
+    def detect_and_track(self, frame): #detect and track players in the frame
+        result = self.model(frame, conf = 0.35) #perform detection
 
+        detections = sv.Detections.from_ultralytics(result[0]) #convert to supervision format
+        tracked_detections = self.tracker.update_with_detections(detections) #update tracker with detections
+
+        return tracked_detections #return tracked detections
+    
     def process_video(self, video_path, output_path): #process the video and save the output
         cap = cv2.VideoCapture(video_path)
 
@@ -46,8 +48,8 @@ class PlayerDetector:
             if not ret:
                 break
 
-            boxes = self.detect_players(frame)
-            frame = self.draw_boxes(frame, boxes)
+            tracked_detections = self.detect_and_track(frame)
+            frame = self.draw_tracked_objects(frame, tracked_detections)
             out.write(frame)
             frame_count += 1
             if frame_count % 30 == 0:
@@ -57,7 +59,49 @@ class PlayerDetector:
         out.release()
         print("Processing complete. Output saved to", output_path)
 
- 
+    def draw_tracked_objects(self, frame, tracked_detections): #draw tracked objects on the frame
+        if len(tracked_detections) == 0:
+            return frame
+        
+        colors = {
+            0: (0, 255, 0),  # Green for ball
+            1: (255, 0, 0),  # Blue for gk classes
+            2: (0, 0, 255),   # Red for player classes
+            3: (255, 255, 0) # Yellow for ref classes
+        }
+
+        class_names = {
+            0: 'ball',
+            1: 'goalkeeper',
+            2: 'player',
+            3: 'referee'
+            }
+
+        for detection in tracked_detections:
+            #Extract data from supervision format
+            bbox = detection[0]
+            confidence = detection[2]
+            class_id = int(detection[3])
+            tracker_id = detection[4] if detection[4] is not None else -1
+
+            x1, y1, x2, y2 = map(int, bbox) # Convert to integers
+
+            color = colors.get(class_id, (0, 255, 0))
+            label = class_names.get(class_id, 'unknown')
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+
+            if tracker_id != -1:
+                text = f'{label} ID:{tracker_id} {confidence:.2f}'
+            else:
+                text = f'{label} {confidence:.2f}'
+            
+            cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+        return frame
+
+            
+
+'''
     def draw_boxes(self, frame, boxes): #draw bounding boxes on the frame
         if boxes is None or len(boxes) == 0:
             return frame
@@ -91,12 +135,11 @@ class PlayerDetector:
             text = f'{label} {confidence:.2f}'
             cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
         return frame
-    
+    '''
 if __name__ == "__main__":
-    detector = PlayerDetector()
     video_path = '/Users/noe/soccer-coach-ai/data/videos/video.mp4'
-    custom_model_path = '/Users/noe/soccer-coach-ai/models/best.pt'
-    if os.path.exists(custom_model_path):
-        detector = PlayerDetector(use_custom_model=True, model_path=custom_model_path)
-        output_path = '/Users/noe/soccer-coach-ai/data/videos/output_video.mp4'
+    model_path = '/Users/noe/soccer-coach-ai/models/best.pt'
+    if os.path.exists(model_path):
+        detector = PlayerDetector(model_path=model_path)
+        output_path = '/Users/noe/soccer-coach-ai/data/outputs/output_video.mp4'
         detector.process_video(video_path, output_path)
